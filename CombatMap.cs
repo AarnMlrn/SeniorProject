c0a071b2e50e6476a591aca96f8dcc74543ed6ac
Snapps <snapples64@gmail.com>
2023-09-27 20:38:22 -0700

    Initial upload

---
+++ CombatMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class CombatMap
+{
+
+	public CombatMap()
+	{
+	
+
+	}
+}
---
+++ main.cs
@@ -0,0 +1,13 @@
+// Hello World! program
+using System;
+
+namespace main
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
